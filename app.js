ccbfe940583ea8953eafa4bba71e2c54efee263e
maxi852 <maxi.rocca99@gmail.com>
2021-11-11 22:11:05 -0300

    Procfile agregado

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando en el puerto 3000');
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('Servidor funcionando en el '+ process.env.PORT);
 });
 
 app.get('/', (req,res)=>{
